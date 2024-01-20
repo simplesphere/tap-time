03346cb44ecfce83ed5096d8c16b7de62b0aa155
simplesphere <anoop.jeewoolall@gmail.com>
2024-01-20 17:39:52 -0500

    chore: fixed prettierrc + added footerButtons + action functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { useState } from "react";
+import FooterButtons from "@/components/FooterButtons"
 
 function App() {
+	const [buttonAction, setButtonAction] = useState<string>('');
+
   return (
-    <div className="h-screen w-screen flex items-center justify-center">
-      <h1 className="text-3xl">Hello World</h1>
+    <div className="h-screen w-screen flex items-center justify-center p-8">
+			<div className="max-w-md w-full h-full flex flex-col space-y-4">
+				<div className="flex flex-grow border border-slate-600 p-4 rounded-md">
+					{buttonAction}
+				</div>
+				<FooterButtons handleButton={setButtonAction} />
+			</div>
     </div>
   )
 }
---
+++ src/components/FooterButtons.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+type TProps = {
+	handleButton: (action: string) => void;
+};
+
+enum ButtonAction {
+	RECORD = 'RECORD',
+	HISTORY = 'HISTORY'
+}
+
+export default function FooterButtons({ handleButton }: TProps) {
+	const [activeButton, setActiveButton] = useState<string | null>(null); 
+
+	const buttons = [
+		{ action: ButtonAction.RECORD, color: 'red', text: 'Record' },
+		{ action: ButtonAction.HISTORY, color: 'yellow', text: 'History' }
+	];
+
+	const handleButtonAction = (action: string) => {
+		handleButton(action);
+		setActiveButton(action);
+	};
+
+	return (
+		<div className="flex space-x-4">
+			{buttons.map(button => (
+				<button
+					key={button.action}
+					className={`flex-1 p-2 border border-${button.color}-500 text-${button.color}-500 rounded-md ${
+						activeButton === button.action ? `bg-${button.color}-500 text-white` : `hover:bg-${button.color}-500 hover:text-white`
+					}`}
+					onClick={() => handleButtonAction(button.action)}
+				>
+					{button.text}
+				</button>
+			))}
+		</div>
+	);
+}
